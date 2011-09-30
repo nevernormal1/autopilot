a875e07d311bc843523a091ccebd09e89ca3b55a
Jeff Talbot <jeff.a.talbot@gmail.com>
2011-09-30 15:34:05 -0500

    Added validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,9 @@
 class Project < ActiveRecord::Base
+  validates_presence_of :pivotal_id
+  validates_presence_of :name
+  validates_presence_of :token
+  validates_presence_of :email
+
   def backlog
     project.backlog
   end
